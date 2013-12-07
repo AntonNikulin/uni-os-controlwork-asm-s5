5fb23e78dc07a9ddb33fe8971b26fb4182e76345
Anton Nikulin <dr.tallin@gmail.com>
2013-12-07 23:05:47 +0200

    +func, loop

--- asm/asm.cpp
+++ asm/asm.cpp
@@ -5,34 +5,51 @@
 #include "stdio.h"
 #include "iostream"
 
+/* function declaration */
+int sumArr (int arr[], int len);
+
 // 6.	Найти сумму элементов массива, меньших 20
 int _tmain(int argc, _TCHAR* argv[])
 {
 	
 	int num[] = {9,10};
-		__asm{
+	sumArr(num,2);	
+		
+	return 0;
+}
+
+int sumArr (int arr[], int len){
+	__asm{
+		mov ecx, len  ;loop counter
+
+	l1:
+		mov ebx, arr
+		mov eax, [ebx]
+		add ebx, 4
+
+
+		loop l1
+
+	}
+}
+
+/*__asm{
 			lea ebx, num
 			add ebx, 4
 			mov eax,[ebx]
 			mov ebx, 3
 
-			mov ecx, 4
+			mov ecx, 1 ;loop counter
 
 lp:
 			cmp eax, 12
-			jge l1
+;			jge l1
 			add eax, 1
 			loop lp
-			mov num, eax
 
 l1:
 
 				inc ebx
 				jmp lp
 
-	}
-		/*std::cout<<num;
-		std::cin>>num;*/
-	return 0;
-}
-
+	}*/
