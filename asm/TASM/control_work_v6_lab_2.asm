cd28b95b4bea8fa23dbb0042d1e8452c27c697b2
Anton Nikulin <dr.tallin@gmail.com>
2013-12-08 18:51:55 +0200

    + comment in TASM ver.

--- asm/TASM/control_work_v6_lab_2.asm
+++ asm/TASM/control_work_v6_lab_2.asm
@@ -8,35 +8,35 @@ len dd 10 ;arr number elem
 cnst dd 20 ;compare to
 res dd 0
 
-codeseg
+codeseg			;init block
 begin :   		mov ecx, [len]        ;loop counter
-                xor edx,edx
-                mov ebx, offset arr
+                xor edx,edx				;edx to 0
+                mov ebx, offset arr		; move adress of the first element in array to ebx
 
 
 
-
-LP1:
+				
+LP1:			;main loop	
                 
-                mov eax, [ebx]  ;arr value to eax
+                mov eax, [ebx]  		;arr value to eax
 
 
-                add ebx, 4                ;next arr value                
-                dec ecx                
+                add ebx, 4                ;get next arr value                
+                dec ecx                		;decrement loop counter
 
 
-                cmp eax, [cnst]
-                jl SUM
-                jge LP1
+                cmp eax, [cnst]			;if array value 
+                jl SUM					;less than variable cnst jump to SUM 
+                jge LP1					;if value greater or equal: next loop iteration
+
+				
+SUM:			;sum array element less than cnst	
+                add edx, eax			;in edx is arr sum, add netxt value to it 
+                cmp ecx, 0				;if number of loop iteration 
+                jg LP1					;greater than 0, start next loop iter
 
 
-SUM:
-                add edx, eax
-                cmp ecx, 0
-                jg LP1
-
-
-                mov [res], edx
+                mov [res], edx			;save result in variable res
 		
 		push 0
 		call ExitProcess
